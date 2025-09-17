6581cea100bf592c39acf716da79c73b6968bf7e
jordyvstim <jordyvanschijndel@icloud.com>
2025-09-17 14:32:24 +0200

    my fifth commit

--- helloworld.c
+++ helloworld.c
@@ -2,10 +2,6 @@
 #include <stdio.h>
 int main()
 {
-<<<<<<< HEAD
-	printf("Hello, Galaxy!");
-=======
-	printf("Hello, Solarsystem!");
->>>>>>> 21d73e44f3de1d6f2018b93dd29049c2dba50ec3
+	printf("Hello, Galaxy and Solarsystem AND multiverse!");
 	return 0;
 }
